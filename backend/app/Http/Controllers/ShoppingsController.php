b9cdbb88f50e84709d6f1758b991b997966c87cd
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-04-07 21:12:03 -0600

    Shopping controller stable

--- backend/app/Http/Controllers/ShoppingsController.php
+++ backend/app/Http/Controllers/ShoppingsController.php
@@ -3,55 +3,100 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Shoppings;
+use App\Models\Videogames;
+use \Illuminate\Database\QueryException;
+use \Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ShoppingsController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $perPage = 10;
+        $page = $request->query('page', 1);
+        $shoppings = Shoppings::paginate($perPage, ['*'], 'page', $page);
+    
+        if ($page > $shoppings->lastPage()) {
+            $response = [
+                'status' => 'error',
+                'message' => 'Page not found'
+            ];
+            return response()->json($response, 404);
+        }
+    
+        $shoppingsWithVideogames = $shoppings->map(function($shopping) {
+            $videogame = Videogames::findOrFail($shopping->id_videogame);
+            return [
+                'id' => $shopping->id,
+                'fullName' => $shopping->fullName,
+                'email' => $shopping->email,
+                'videogame' => [
+                    'id' => $videogame->id,
+                    'title' => $videogame->title,
+                    'cover' => $videogame->cover,
+                    'description' => $videogame->description,
+                    'price' => $videogame->price,
+                ]
+            ];
+        });
+    
+        return $shoppingsWithVideogames;
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $shoppings = new Shoppings;
+            $shoppings->fullName = $request->input('fullName');
+            $shoppings->email = $request->input('email');
+            $shoppings->id_videogame = $request->input('id_videogame');
+            $shoppings->save();
+        } catch (QueryException $e) {
+            $response = [
+                'status' => 'error',
+                'message' => 'Error, check your information',
+            ];
+            return response()->json($response, 400);
+        }
+        $response = [
+            'status' => 'success',
+            'message' => 'Shopping created successfully',
+            'data' => [
+                'fullName' => $shoppings->fullName,
+                'email' => $shoppings->email,
+                'id_videogame' => $shoppings->id_videogame,
+            ],
+        ];
+        return response()->json($response, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request, string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+        try {
+            $shoppings = Shoppings::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            $response = [
+                'status' => 'error',
+                'message' => 'Shopping not found'
+            ];
+            return response()->json($response,404);
+        }
+        $response = [
+            'status' => 'success',
+            'message' => 'Shopping founded',
+            'data' => $shoppings
+        ];
+        return response()->json($response, 201);
     }
 
     /**
@@ -59,6 +104,25 @@ class ShoppingsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try{
+            $shoppings = Shoppings::findOrFail($id);
+        }catch(ModelNotFoundException $e){
+            $response = [
+                'status' => 'error',
+                'message' => 'Shopping not found',
+            ];
+            return response()->json($response, 404);
+        }
+            $shoppings -> delete();
+            $response = [
+                'status' => 'success',
+                'message' => 'Shopping deleted successfully',
+                'data' => [
+                    'id' => $shoppings->id,
+                    'fullName' => $shoppings->fullName,
+                    'id_videogame'=>$shoppings->id_videogame
+                ],
+            ];
+            return response()->json($response, 201);
     }
 }
